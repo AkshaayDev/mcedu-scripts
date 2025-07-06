83ef3e7a05c71bbc9c0a9e766aaf865f1bdfced4
Akshaay Kaldev <163286127+AkshaayDev@users.noreply.github.com>
2025-07-06 17:23:56 +0800

    Add header comments

--- family-fun-time.ts
+++ family-fun-time.ts
@@ -1,4 +1,22 @@
-// This script gives the player and the agent fun powers and abilities.
+/*
+
+This script gives the player and the agent fun powers and abilities.
+These abilities include powers, teleportation, grenades and weapons.
+To use these abilities, equip respective item and interact:
+* Iron Door       : Teleport agent to player
+* Oak Door        : Teleport player to agent
+* Skeleton Skull  : Agent disappears with an explosion particle
+* Gunpowder       : Prime a TNT at the agent's location
+* Charcoal        : Throw smoke grenade
+* Dragon's Breath : Throw poison grenade
+* Redstone        : Throw thumper grenade
+* Fire Charge     : Throw molotov cocktail
+* Echo Shard      : Teleport forward until obstacle (max 20 blocks)
+* Feather         : Teleport up 20 blocks
+* Sugar           : Temporary superpowers
+* Lightning Rod   : Orbital strike
+
+*/
 const PI: number = 3.141592653589793
 const GRAVITY: number = 0.1
 const THROW_ITERATIONS: number = 50
@@ -34,7 +52,7 @@ function playerThrow(particle: Particle): Position {
 // Repeat a function continuously for a duration with a default interval of 0
 function repeatFor(f: Function, duration: number, interval: number = 0): void {
     let running: boolean = true
-    loops.runInBackground(function() {
+    loops.runInBackground(function () {
         loops.pause(duration)
         running = false
     })
@@ -49,29 +67,29 @@ function asyncPlaySound(sound: Sound): void {
 }
 
 // Teleports the agent to you
-player.onItemInteracted(Item.IronDoor, function() {
+player.onItemInteracted(Item.IronDoor, function () {
     asyncPlaySound(Sound.Trident)
     agent.teleport(pos(0, 0, 0), positions.toCompassDirection(player.getOrientation()))
 })
 // Teleports you to the agent
-player.onItemInteracted(Item.OakDoor, function() {
+player.onItemInteracted(Item.OakDoor, function () {
     asyncPlaySound(Sound.Trident)
     player.teleport(agent.getPosition())
 })
 // Agent disappears with an explosion particle
-player.onItemInteracted(Block.SkeletonSkull, function() {
+player.onItemInteracted(Block.SkeletonSkull, function () {
     mobs.spawnParticle(Particle.ExplosionHuge, agent.getPosition())
     mobs.kill(mobs.target(TargetSelectorKind.MyAgent))
 })
 // Prime a TNT at the agent's location
-player.onItemInteracted(Item.Gunpowder, function() {
+player.onItemInteracted(Item.Gunpowder, function () {
     asyncPlaySound(Sound.Fuse)
     mobs.spawn(ProjectileMob.PrimedTnt, agent.getPosition())
 })
 // Throw a smoke grenade that hides mobs inside
-player.onItemInteracted(Item.Charcoal, () => loops.runInBackground(function() {
+player.onItemInteracted(Item.Charcoal, () => loops.runInBackground(function () {
     let pos: Position = playerThrow(Particle.SmokeCampfire)
-    repeatFor(function() {
+    repeatFor(function () {
         asyncPlaySound(Sound.Fizz)
         let selector: TargetSelector = mobs.near(mobs.target(TargetSelectorKind.AllEntities), pos, 5)
         player.execute(`effect ${selector.toString()} invisibility 2 1 true`)
@@ -79,9 +97,9 @@ player.onItemInteracted(Item.Charcoal, () => loops.runInBackground(function() {
     }, 15000)
 }))
 // Throw a poison gas grenade that poisons mobs inside
-player.onItemInteracted(Item.DragonSBreath, () => loops.runInBackground(function() {
+player.onItemInteracted(Item.DragonSBreath, () => loops.runInBackground(function () {
     let pos: Position = playerThrow(Particle.SmokeLlamaSpit)
-    repeatFor(function() {
+    repeatFor(function () {
         asyncPlaySound(Sound.Fizz)
         let selector: TargetSelector = mobs.near(mobs.target(TargetSelectorKind.AllEntities), pos, 5)
         mobs.applyEffect(Effect.Poison, selector, 2, 100)
@@ -90,7 +108,7 @@ player.onItemInteracted(Item.DragonSBreath, () => loops.runInBackground(function
     }, 15000)
 }))
 // Throw a thumper grenade that deals 3 pulses of damage to mobs inside
-player.onItemInteracted(Item.Redstone, () => loops.runInBackground(function() {
+player.onItemInteracted(Item.Redstone, () => loops.runInBackground(function () {
     let pos: Position = playerThrow(Particle.SmokeBasic)
     let selector: TargetSelector = mobs.near(mobs.target(TargetSelectorKind.AllEntities), pos, 5)
     asyncPlaySound(Sound.Blaze)
@@ -104,7 +122,7 @@ player.onItemInteracted(Item.Redstone, () => loops.runInBackground(function() {
     }
 }))
 // Throw a molotov cocktail that sets an area on fire for 10 seconds
-player.onItemInteracted(Item.Fireball, () => loops.runInBackground(function() {
+player.onItemInteracted(Item.Fireball, () => loops.runInBackground(function () {
     let pos: Position = playerThrow(Particle.FireVapor)
     mobs.spawnParticle(Particle.ExplosionCauldron, positions.add(pos, world(0, 2, 0)))
     let corner1: Position = positions.add(pos, world(-2, -1, -2))
@@ -114,7 +132,7 @@ player.onItemInteracted(Item.Fireball, () => loops.runInBackground(function() {
     blocks.replace(Block.Air, Block.Fire, corner1, corner2)
 }))
 // Teleport forward until the path is blocked to if reached 20 blocks
-player.onItemInteracted(Item.EchoShard, function() {
+player.onItemInteracted(Item.EchoShard, function () {
     asyncPlaySound(Sound.ElderGuardian)
     let pos: Position = player.position()
     let orientation: number = (player.getOrientation() + 360) % 360 * PI / 180;
@@ -135,12 +153,12 @@ player.onItemInteracted(Item.EchoShard, function() {
     }
 })
 // Teleport the player 20 blocks up
-player.onItemInteracted(Item.Feather, function() {
+player.onItemInteracted(Item.Feather, function () {
     asyncPlaySound(Sound.Click)
     player.teleport(pos(0, 20, 0))
 })
 // Gives player powers
-player.onItemInteracted(Item.Sugar, function() {
+player.onItemInteracted(Item.Sugar, function () {
     asyncPlaySound(Sound.LevelUp)
     const duration: number = 10
     const effects: [Effect, number][] = [
@@ -157,7 +175,7 @@ player.onItemInteracted(Item.Sugar, function() {
     })
 })
 // Call an orbital strike to strike the area around the agent with lightning
-player.onItemInteracted(Block.LightningRod, function() {
+player.onItemInteracted(Block.LightningRod, function () {
     const rad: number = 3
     let pos: Position = agent.getPosition()
     for (let i = 0; i < 4; i++) {
